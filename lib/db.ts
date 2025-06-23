7d87b8688d8b877c5eb4274c4b1d25cf5d457b0b
Akshay GH <dualdeveloper002@gmail.com>
2025-06-23 11:33:32 +0530

    secured

--- lib/db.ts
+++ lib/db.ts
@@ -1,17 +1,17 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const MONGODB_URI = 'mongodb+srv://dualdeveloper002:akshay123@cluster0.cxhro.mongodb.net/crowd'
+const MONGODB_URI = process.env.DB_URL;
 
 if (!MONGODB_URI) {
-  throw new Error('Please define the MONGODB_URI environment variable');
+  throw new Error("Please define the MONGODB_URI environment variable");
 }
 
 async function connectDB() {
   try {
-    await mongoose.connect(MONGODB_URI);
-    console.log('MongoDB connected successfully');
+    await mongoose.connect(`${MONGODB_URI}`);
+    console.log("MongoDB connected successfully");
   } catch (error) {
-    console.error('MongoDB connection error:', error);
+    console.error("MongoDB connection error:", error);
     process.exit(1); // Exit process with failure
   }
 }
